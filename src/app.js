a4887e5f6364c361132103393a7acc8bd011a8e9
Myo Thant Kyaw <myothantkyaw21@gmail.com>
2020-04-14 21:09:36 +0630

    .....

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
 
 import { Mesh, Sphere, RedIntegerFormat } from 'three';
 
-
 var scene, camera, renderer, orbit;
 var perspective_camera;
 var ball;
@@ -13,6 +12,12 @@ var pointLight;
 var toposphere;
 let layers = []
 const earthRadius = 10;
+const twoPI = Math.PI * 2;
+var plane, satellite;
+var isCloudLoaded = false;
+var isPlaneLoaded = false;
+var isSatelliteLoaded = false;
+var earth;
 
 let altitudeMeter;
 var clipPlanes = [
@@ -41,7 +46,7 @@ function init() {
 
     renderer.setSize(position_info.width, position_info.height);
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setClearColor(0x333333);
+    renderer.setClearColor(0x222222);
     view_3d.appendChild(renderer.domElement);
 
     perspective_camera = new THREE.PerspectiveCamera(45, position_info.width / position_info.height, 1, 1000);
@@ -65,7 +70,7 @@ function init() {
     // orbit.dampingFactor = .18;
 
     //// start adding graphics
-    // scene.add(new THREE.AxesHelper(100))
+    // scene.add(new THREE.AxesHelper(30))
 
     // var ambient = new THREE.AmbientLight(0xffffff, 0.3);
     // scene.add(ambient);
@@ -107,30 +112,33 @@ function init() {
 
     scene.add(spotLight1)
 
-    var sphereGeom = new THREE.SphereGeometry(earthRadius, 100, 16);
+    var sphereGeom = new THREE.SphereBufferGeometry(earthRadius, 100, 100);
 
     // var earthTexture = THREE.ImageUtils.loadTexture( 'images/earth.jpg' );
     var earthMaterial = new THREE.MeshBasicMaterial({});
-    var earth = new THREE.Mesh(sphereGeom, earthMaterial);
+    earth = new THREE.Mesh(sphereGeom, earthMaterial);
     earth.position.set(0, 0, 0);
+    // earth.rotateX(.5)
+    // earth.rotateX(Math.PI / 2)
+    
     scene.add(earth);
 
     // create custom material from the shader code above
-    //   that is within specially labeled script tags
+    // that is within specially labeled script tags
 
-    var troposhpere = addLayer(earth, earthRadius, earthRadius + 2, 0xff0000)
+    var troposhpere = addLayer(earth, earthRadius, earthRadius + 2, 0xf2b500)
     layers.push(troposhpere);
 
-    var stratoshpere = addLayer(earth, earthRadius + 2, earthRadius + 4, 0x00ff00)
+    var stratoshpere = addLayer(earth, earthRadius + 2, earthRadius + 4, 0xC4B3C2)
     layers.push(stratoshpere);
 
-    var mesoshpere = addLayer(earth, earthRadius + 4, earthRadius + 6, 0x0000ff)
+    var mesoshpere = addLayer(earth, earthRadius + 4, earthRadius + 6, 0x85a7f7)
     layers.push(mesoshpere);
 
-    var termoshpere = addLayer(earth, earthRadius + 6, earthRadius + 8, 0x00f0ff)
+    var termoshpere = addLayer(earth, earthRadius + 6, earthRadius + 8, 0x033B7B)
     layers.push(termoshpere);
 
-    var exoshpere = addLayer(earth, earthRadius + 8, earthRadius + 10, 0xfff0ff)
+    var exoshpere = addLayer(earth, earthRadius + 8, earthRadius + 10, 0x110C22)
     layers.push(exoshpere);
 
     for (var i = layers.length - 1; i >= 0; i--) {
@@ -143,7 +151,13 @@ function init() {
         function (texture) {
             earthMaterial.map = texture;
             earthMaterial.needsUpdate = true;
+            
+            isCloudLoaded = true;
             render();
+            
+            if(isSatelliteLoaded && isPlaneLoaded){
+                animate();
+            }
         },
         undefined,
         function (err) {
@@ -162,13 +176,19 @@ function init() {
     var gltf_loader = new GLTFLoader();
     gltf_loader.load("3d_models/satellite_1.glb",
         function (gltf) {
-            var plane = gltf.scene;
-            plane.position.set(0, earthRadius + 4, 2)
-            plane.scale.set(.2, .2, .2)
-            plane.rotateY(-Math.PI / 3)
-            plane.rotateZ(-.4)
-            scene.add(plane)
+            satellite = gltf.scene;
+            satellite.position.set(0, earthRadius + 4, 2)
+            satellite.scale.set(.15, .15, .15)
+            satellite.rotateY(-Math.PI / 3)
+            satellite.rotateZ(-.4)
+            scene.add(satellite)
             render();
+
+            isSatelliteLoaded = true;
+
+            if(isCloudLoaded && isPlaneLoaded){
+                animate();
+            }
         }, undefined,
         function (err) {
             console.error(err);
@@ -176,15 +196,22 @@ function init() {
     )
 
     var gltf_loader_airplane = new GLTFLoader();
-    gltf_loader_airplane.load("3d_models/boeing787_8.glb",
+    gltf_loader_airplane.load("3d_models/B_787_8.glb",
         function (gltf) {
-            var plane = gltf.scene;
-            plane.position.set(0, earthRadius + 7, 2)
-            plane.scale.set(1.5, 1.5, 1.5)
-            plane.rotateY(-Math.PI / 3)
-            plane.rotateZ(-.4)
+            plane = gltf.scene;
+            plane.position.set(0, earthRadius + 7, 0)
+            plane.scale.set(.9, .9, .9)
+            plane.rotation.y = -(Math.PI / 2)
+            plane.rotation.z = -.1
+            plane.rotateX(-Math.PI / 2)
             scene.add(plane)
             render();
+
+            isPlaneLoaded = true;
+
+            if(isCloudLoaded && isSatelliteLoaded){
+                animate();
+            }
         }, undefined,
         function (err) {
             console.error(err);
@@ -204,16 +231,15 @@ function init() {
     var helpers = new THREE.Group();
     helpers.add(new THREE.PlaneHelper(clipPlanes[0], 100, 0xff0000));
 
-    camera.position.set(0, earthRadius + 3, 20);
+    camera.position.set(0, earthRadius + 3, 25);
     orbit.target = new THREE.Vector3(0, earthRadius + 3, 0);
     orbit.update();
 
     // altitudeMeter = new THREE
 
-
     addAltitudeLabels();
+    addLayerLabels();
     render();
-    // animate();
 }
 
 function render() {
@@ -226,40 +252,79 @@ function render() {
     renderer.render(scene, camera);
 }
 
+let clock = new THREE.Clock();
+var elapsed;
+const time_to_orbit_plane = 10;
+var angForPlane;
+var angForPlane1;
+var lastElapsed = 0;
+var delta;
+var planeRotationAxis = new THREE.Vector3(0,  3, -1).normalize()
+var xForPlane, yForPlane;
+var xForSat, yForSat;
+
 function animate() {
-    render();
+    
     requestAnimationFrame(animate);
+
+    elapsed = clock.getElapsedTime();
+
+    delta = elapsed - lastElapsed;
+    
+    angForPlane = (delta / time_to_orbit_plane) * twoPI;
+    angForPlane1 = (elapsed / time_to_orbit_plane) * twoPI;
+
+    xForPlane = Math.cos(angForPlane1);
+    yForPlane = Math.sin(angForPlane1);
+    
+    plane.rotateX(angForPlane)
+
+    plane.position.x = xForPlane * (earthRadius + 2.5);
+    plane.position.y = yForPlane * (earthRadius + 2.5);
+    plane.position.z = 1;
+
+    xForSat = Math.cos(angForPlane1 - 2.2);
+    yForSat = Math.sin(angForPlane1 - 2.2);
+
+    satellite.rotateX(angForPlane)
+
+    satellite.position.x = xForSat * (earthRadius + 5);
+    satellite.position.y = yForSat * (earthRadius + 5);
+    satellite.position.z = 1;
+
+    // earth.rotation.y = -angForPlane1 / 4;
+    earth.rotateOnWorldAxis( planeRotationAxis, angForPlane / 6)
+
+    lastElapsed = elapsed;
+    render();
 }
 
 function addLayer(mesh, innerRadius, outerRadius, color) {
-    var material = new THREE.ShaderMaterial(
-        {
-            uniforms:
-            {
-                "c": { type: "f", value: 1 },
-                "p": { type: "f", value: 0 },
-                glowColor: { type: "c", value: new THREE.Color(color) },
-                viewVector: { type: "v3", value: camera.position }
-            },
-            vertexShader: document.getElementById('vertexShader').textContent,
-            fragmentShader: document.getElementById('fragmentShader').textContent,
-            side: THREE.BackSide,
-            blending: THREE.AdditiveBlending,
-            transparent: true,
-            opacity: .2
-        });
+    // var material = new THREE.ShaderMaterial(
+    //     {
+    //         uniforms:
+    //         {
+    //             "c": { type: "f", value: 1 },
+    //             "p": { type: "f", value: 0 },
+    //             glowColor: { type: "c", value: new THREE.Color(color) },
+    //             viewVector: { type: "v3", value: camera.position }
+    //         },
+    //         vertexShader: document.getElementById('vertexShader').textContent,
+    //         fragmentShader: document.getElementById('fragmentShader').textContent,
+    //         side: THREE.BackSide,
+    //         blending: THREE.AdditiveBlending,
+    //         transparent: true,
+    //         opacity: .2
+    //     });
 
     var basicMat = new THREE.MeshBasicMaterial({
-        color: color, transparent: true, opacity: .2,
+        color: color, transparent: true, opacity: .4,
         side: THREE.DoubleSide,
         depthWrite: false,
 
     })
-    var layer = new THREE.Mesh(new THREE.RingGeometry(innerRadius, outerRadius, 60), basicMat);
+    var layer = new THREE.Mesh(new THREE.RingBufferGeometry(innerRadius, outerRadius, 100), basicMat);
     layer.position.copy(mesh.position);
-    // layer.scale.multiplyScalar(1.2);
-    // scene.add(layer);
-    // layer.rotateY(Math.PI / 2)
 
     return layer;
 }
@@ -270,9 +335,8 @@ let altLoc = [2, 4, 6, 8]
 function addAltitudeLabels(){
     var loader = new THREE.FontLoader();
     loader.load('styles/Zawgyi-One_Regular.json', function(font) {
-        // label_font = font;
 
-        var color = 0xff0000;
+        var color = 0x779efb;
 
         var matLite = new THREE.MeshBasicMaterial({
             color: color,
@@ -284,7 +348,7 @@ function addAltitudeLabels(){
         for(var i = 0; i < altLabels.length; i++){
             var message = altLabels[i];
 
-            var shapes = font.generateShapes(message, .2);
+            var shapes = font.generateShapes(message, .3);
     
             var geometry = new THREE.ShapeBufferGeometry(shapes);
     
@@ -292,17 +356,61 @@ function addAltitudeLabels(){
     
             var xMid = - (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
     
-            geometry.translate(xMid - 1, -.1, 0);
-            // geometry.rotateZ(Math.PI )
-            // geometry.rotateX(-Math.PI / 2)
-            // geometry.rotateZ(Math.PI / 4)
+            geometry.translate(xMid / 2, -.2, 0);    
+
+            var text_alt = new THREE.Mesh(geometry, matLite);
+            
+            text_alt.rotateZ( .4);
+            
+            
+            var x = Math.cos(Math.PI / 2 + .3);
+            var y = Math.sin(Math.PI / 2 + .3);
+            text_alt.position.y = y * (earthRadius + altLoc[i]);
+            text_alt.position.x = x * (earthRadius + altLoc[i]);
+            text_alt.position.z = .1;
+            
+            scene.add(text_alt);
+        }
+    })
+}
+
+let layerLabels = ["ထရိုပိုစဖီးယား", "အိုဇုန္းလႊာ", "စတရာတိုစဖီးယား", "မီဆိုစဖီးယား", "သာမိုစဖီးယား", "အိပ္ဇိုစဖီးယား"]
+let layerLoc = [1, 2.5, 3.3, 5, 7, 9]
+
+function addLayerLabels(){
+    var loader = new THREE.FontLoader();
+    loader.load('styles/Zawgyi-One_Regular.json', function(font) {
+
+        var color = 0x70c409;
+
+        var matLite = new THREE.MeshBasicMaterial({
+            color: color,
+            transparent: true,
+            opacity: 1,
+            side: THREE.DoubleSide
+        });
+
+        for(var i = 0; i < layerLabels.length; i++){
+            var message = layerLabels[i];
+
+            var shapes = font.generateShapes(message, .3);
+    
+            var geometry = new THREE.ShapeBufferGeometry(shapes);
+            geometry.computeBoundingBox();
     
             var text_alt = new THREE.Mesh(geometry, matLite);
-            text_alt.position.y = earthRadius + altLoc[i];
 
-            // text_alt.position.x = side_length_y / 2 + .1;
-            // text_alt.position.y = side_length_x / 2 + .1;
+            var xMid = - (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
+            geometry.translate(xMid / 2, -.1, 0);
+
+            text_alt.rotateZ(- .4);
             
+            var x = Math.cos(Math.PI / 2 - .3);
+            var y = Math.sin(Math.PI / 2 - .3);
+            text_alt.position.y = y * (earthRadius + layerLoc[i]);
+            text_alt.position.x = x * (earthRadius + layerLoc[i]);
+            text_alt.position.z = .1;
+
             scene.add(text_alt);
         }
     })
